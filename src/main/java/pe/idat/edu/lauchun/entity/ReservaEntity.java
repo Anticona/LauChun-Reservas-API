023fccfb166ef5f7dc52eb268acbb2e43a66bd06
CamposVillalobos <114685467+CamposVillalobos@users.noreply.github.com>
2022-11-28 19:25:46 -0500

    Imports de  Reserva
    Implementacion de imports restantes

--- src/main/java/pe/idat/edu/lauchun/entity/ReservaEntity.java
+++ src/main/java/pe/idat/edu/lauchun/entity/ReservaEntity.java
@@ -1,5 +1,11 @@
 package pe.idat.edu.lauchun.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
